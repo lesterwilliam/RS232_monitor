960e038ef0f696c801e5500af73d0195ff68940c
lesterwilliam <adrian.schwizgebel@gmail.com>
2018-07-17 07:45:14 +0200

    Emergency commit in the morning

--- RS232_monitor.py
+++ RS232_monitor.py
@@ -1,7 +1,22 @@
+import warnings
 import serial
+import serial.tools.list_ports
 
-ser = serial.Serial("COM1", 9600, timeout=0.02)
+arduino_ports = [
+	p.device
+	for p in serial.tools.list_ports.comports()
+	if 'CH340' in p.description
+]
+if not arduino_ports:
+	raise IOError("No Arduino found")
+if len(arduino_ports) > 1:
+	warnings.warn('Multiple Arduinos found - using the first')
 
+ser = serial.Serial(arduino_ports[0], 9600, timeout=0.02)
+ports = list(serial.tools.list_ports.comports())
+
+print ('Started...')
+	
 while True:
 	command = ser.readline()
 	if command:
